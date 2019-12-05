41d2cb13cd0a50b17ccef512decae217b2b74afd
9631061 <9631061>
2019-12-05 04:09:13 -0800

    change code of first problem

--- 1/1.c
+++ 1/1.c
@@ -1,46 +1,3 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-
-// int main(int argc, char *argv[])
-// {
-//     int fd[2];
-//     int fd2 [2];
-//     int val = 0;
-
-//     // create pipe descriptors
-//     pipe(fd);
-
-//     // fork() returns 0 for child process, child-pid for parent process.
-//     if (fork() != 0)
-//     {
-//         // parent: writing only, so close read-descriptor.
-//         close(fd[0]);
-
-//         // send the value on the write-descriptor.
-//         val = 100;
-//         write(fd[1], &val, sizeof(val));
-//         printf("I am parent : \n I send value: %d\n", val);
-
-//         // close the write descriptor
-//         close(fd[1]);
-//     }
-//     else
-//     {   // child: reading only, so close the write-descriptor
-//         close(fd[1]);
-
-//         // now read the data (will block)
-//         read(fd[0], &val, sizeof(val));
-//         printf("I am child \n I receive value: %d\n", val);
-
-//         // close the read-descriptor
-//         close(fd[0]);
-//     }
-//     return 0;
-// }
-
-
-//--------------------------------
 // C program to demonstrate use of fork() and pipe() 
 #include<stdio.h> 
 #include<stdlib.h> 
@@ -48,28 +5,6 @@
 #include<sys/types.h> 
 #include<string.h> 
 #include<sys/wait.h> 
-
-char * toArray(int number)
-{
-    int n = log10(number) + 1;
-    int i;
-    char *numberArray = calloc(n, sizeof(char));
-    for ( i = 0; i < n; ++i, number /= 10 )
-    {
-        numberArray[i] = number % 10;
-    }
-    return numberArray;
-}
-
-char concat(char a[], char b[]){
-   int lena = strlen(a);
-   int lenb = strlen(b);
-   char con[lena+lenb];
-   con[0] = a;
-   con[lena] = b;
-   return con;
-}
-
   
 int main() 
 { 
@@ -80,12 +15,8 @@ int main()
     int fd1[2];  // Used to store two ends of first pipe 
     int fd2[2];  // Used to store two ends of second pipe 
   
-    char fixed_str[] = "forgeeks.org"; 
     char input_str[100]; 
     pid_t p; 
-    int val; 
-
-    scanf("Enter value : %d" , &val); 
   
     if (pipe(fd1)==-1) 
     { 
@@ -114,9 +45,9 @@ int main()
   
         close(fd1[0]);  // Close reading end of first pipe 
   
-       // send the value on the write-descriptor.
-        write(fd1[1], &val, sizeof(val));
-        printf("I am parent : \n I send value: %d\n", val);
+        // Write input string and close writing end of first 
+        // pipe. 
+        write(fd1[1], input_str, strlen(input_str)+1); 
         close(fd1[1]); 
   
         // Wait for child to send a string 
@@ -136,34 +67,27 @@ int main()
     { 
         close(fd1[1]);  // Close writing end of first pipe 
   
+        // Read a string using first pipe 
+        char concat_str[100]; 
+        read(fd1[0], concat_str, 100); 
+        int i , sum = 0 ; 
+        for (i=0; i<strlen(concat_str); i++ ){
+            if ((concat_str[i] - 48)%2 == 0  )
+                sum += concat_str[i] - 48; 
+        }
+        char fixed_str[] = "The sum of even digits in the input number :"; 
+        char integer_str[100];
+        sprintf(integer_str, "%d", sum);
+        strcat(fixed_str, integer_str);
 
-        // now read the data (will block)
-        read(fd1[0], &val, sizeof(val));
-        printf("I am child \n I receive value: %d\n", val);
-        int i = 0 ; 
-        int sum = 0 ; 
-        int hold = val ; 
-        int digit = 0 ; 
-
-        while (hold > 0 ){
-            digit = hold % 10 ; 
-            if (hold % 2 == 0 )
-                sum += digit; 
-            hold /= 0 ; 
-        }  
-
-
-        char con1 = concat(toArray(val) , fixed_str ); 
-        char result = concat(con1 , toArray(sum)); 
   
-        concat_str[k] = '\0';   // string ends with '\0' 
   
         // Close both reading ends 
         close(fd1[0]); 
         close(fd2[0]); 
   
         // Write concatenated string and close writing end 
-        write(fd2[1], concat_str, strlen(concat_str)+1); 
+        write(fd2[1], fixed_str, strlen(fixed_str)+1); 
         close(fd2[1]); 
   
         exit(0); 
