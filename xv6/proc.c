c046fc9f3cf34e2760472574fabfcbcc1f9739af
9631061 <9631061>
2019-12-06 00:25:30 -0800

    add priority and calculatedPriority to processes and use them in scheduler

--- xv6/proc.c
+++ xv6/proc.c
@@ -326,11 +326,23 @@ scheduler(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
+  long int maxPriority = 0; 
   
   for(;;){
     // Enable interrupts on this processor.
     sti();
 
+    // Loop over process table looking for find highest calculatedPriority.
+    int flag = 0; 
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+      if(flag == 0 ){
+        flag = 1 ; 
+        maxPriority = p->calculatedPriority;
+      }else if(p->calculatedPriority < maxPriority)
+        maxPriority = p->calculatedPriority;
+    }
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
@@ -340,12 +352,15 @@ scheduler(void)
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+      if(p->calculatedPriority == maxPriority){
+        c->proc = p;
+        switchuvm(p);
+        p->state = RUNNING;
+        p->calculatedPriority += p->priority;
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+      }
+     
 
       // Process is done running for now.
       // It should have changed its p->state before coming back.
--- xv6/proc.h
+++ xv6/proc.h
@@ -50,6 +50,8 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int sc_count[40];
+  int priority;
+  long int calculatedPriority;
 };
 
 // Process memory is laid out contiguously, low addresses first:
